35014e75c66c7e5dfb25256d349326fdaa629718
andythierry <andythierry@me.com>
2022-04-17 07:33:08 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import time
+from lsm303d import LSM303D
+import os
+lsm = LSM303D(0x1d)  # Change to 0x1e if you have soldered the address jumper
+
+repertoire="LOGS/"+datetime.now().strftime("%d_%m_%Y")
+try:
+ os.makedirs(repertoire)
+except OSError:
+ if not os.path.isdir(repertoire):
+     raise Exception("Impossible de créer le répertoire de destination")
+
+file=repertoire+'/'+datetime.now().strftime("%d_%m_%Y-%H:%M:%S")
+
+
+fl=open(file, mode='x', buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None)
+while True:
+    xyz = lsm.accelerometer()
+    mgxyz = lsm.magnetometer()
+    print('accelerometre :',xyz)
+    print('magnetometre: ',mgxyz)
+    #print(("{:+06.2f}g : {:+06.2f}g : {:+06.2f}g").format(*xyz))
+    fl.write(("{:+06.2f}g : {:+06.2f}g : {:+06.2f}g").format(*xyz))
+    time.sleep(0.5)
+
+
+
+
+
