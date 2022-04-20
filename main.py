df420d2ec1f4d167b8533d33834cc984abdf45a3
andythierry <andythierry@me.com>
2022-04-20 21:01:29 +0400

    espace entre accel et magneto

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ while True:
 
 
     fl.write(("{:+06.2f} , {:+06.2f} , {:+06.2f}").format(*xyz))
-    fl.write(",")
+    fl.write(" , ")
 
     fl.write(("{:+06.2f} , {:+06.2f} , {:+06.2f}").format(*mgxyz))
     fl.write('\n')
