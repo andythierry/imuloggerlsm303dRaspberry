23aa90f0e34f6abe02d400c24f87dfc03d57214d
andythierry <andythierry@me.com>
2022-04-24 23:02:49 +0400

    Rajoute le timestamp

--- main.py
+++ main.py
@@ -16,16 +16,18 @@ file=repertoire+'/'+datetime.now().strftime("%d_%m_%Y-%H:%M:%S"+".csv")
 
 
 fl=open(file, mode='x', buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None)
-fl.write("accelerometre X,accelerometre Y,accelerometre Z,")
+fl.write("TimeStamp ,accelerometre X,accelerometre Y,accelerometre Z,")
 fl.write("magnetometre X,magnetometre Y,magnetometre Z \n")
 while True:
     xyz = lsm.accelerometer()
     mgxyz = lsm.magnetometer()
-    print('accelerometre :',xyz)
-    print('magnetometre: ',mgxyz)
+    print('TimeStamp', datetime.now().time())
+
+    print('accelerometre :', xyz)
+    print('magnetometre: ', mgxyz)
     #print(("{:+06.2f}g : {:+06.2f}g : {:+06.2f}g").format(*xyz))
 
-
+    fl.write(datetime.now().time())
     fl.write(("{:f},{:f},{:f}").format(*xyz))
     fl.write(",")
 
